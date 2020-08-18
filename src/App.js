4f09d29775379f4078808b73ef4b486ba6ddd9a7
Artyom <r.tem13@gmail.com>
2020-08-18 23:26:27 +0700

    Обработал ошибку отсутствия значения rain.3h и сумму за день осадков прокинул в рендер

--- src/App.js
+++ src/App.js
@@ -9,35 +9,68 @@ const API_KEY="94da30484bb9daa3c3aa4b131328d680";
 class App extends React.Component { //наследует
 
     state = {
-        temp: undefined,
         city: undefined,
-        rain: undefined
-
+        //today
+        temp: undefined,
+        rain: undefined,
+        //tomorrow
+        temp1: undefined,
+        rain1: undefined,
     }
 
     gettingWeather = async (e) => {  //async - страница не перезагружается
         e.preventDefault();
         const api_url = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=Krasnoyarsk &appid=${API_KEY}&units=metric`); // шаблон строки указываем, а не прямой адрес, кавычки косые
         const data = await api_url.json();
-        console.log(data);
+
+        /*1й день*/
+        /*Создаем массив со всеми значениями*/
+        var array = [];
+        for (let i = 0; i <= 8; i++) {
+            try {
+                array[i] = data.list[i].rain['3h'];
+            } catch (e) {
+                array[i] = 0;
+            }
+        }
+
+        /*сумма элементов массива*/
+        function summ() {
+            var sum = 0;
+            for(let i = 0; i < array.length; i++){
+                sum += array[i];
+            }
+            return sum;
+        }
+        var rainSum1 = summ();
+        rainSum1=rainSum1.toFixed(2); // сокращаем до 2х знаков
+
+        /*2й день*/
+        /*обернуть предыдущие 2 цикла в функции с параметрами для дальнешего использования в день 2-5*/
 
         this.setState({
             city: data.city.name,
-            temp: data.list[0].main.temp,
-            rain: data.list[1].rain['3h'] + data.list[2].rain['3h'],
-            // бывает что поля rain нет, так как дождя нет
+            //today
+            temp: data.list[5].main.temp,
+            rain: rainSum1,
+            //tomorrow
         });
     }
 
     render() {
         return (
+
             <div>
                 <Header />
                 <Form weatherMethod={this.gettingWeather}/>
                 <Weather
                     city={this.state.city}
+                    //today
                     temp={this.state.temp}
                     rain={this.state.rain}
+                    //tomorrow
+                    temp1={this.state.temp1}
+                    rain1={this.state.rain1}
                 />
             </div>
         );
@@ -46,11 +79,14 @@ class App extends React.Component { //наследует
 
 export default App; // экспортируем приложение в index.js
 
+/* ========ReadMe=============*/
 /* Weather JSON links
-http://api.openweathermap.org/data/2.5/forecast?q=Krasnoyarsk&appid=94da30484bb9daa3c3aa4b131328d680
-http://api.openweathermap.org/data/2.5/weather?q=Krasnoyarsk&appid=94da30484bb9daa3c3aa4b131328d680
+http://api.openweathermap.org/data/2.5/forecast?q=Krasnoyarsk&appid=94da30484bb9daa3c3aa4b131328d680 - прогноз
+http://api.openweathermap.org/data/2.5/weather?q=Krasnoyarsk&appid=94da30484bb9daa3c3aa4b131328d680 - текущая погодаgit
 */
 
 /* Links on React learning (ItProger)
 https://www.youtube.com/watch?v=Fxl_LU68Q5g
-* */
+*/
+
+/*Запуск скрипта в 00:01, будет брать данные по температуре на 15:00 (5 с циклом 8)*/
--- src/components/weather.js
+++ src/components/weather.js
@@ -5,8 +5,16 @@ class Weather extends React.Component {
         return (
             <div>
                 <p>Город: {this.props.city}</p>
+
+                <p>Сегодня</p>
                 <p>Температура: {this.props.temp}</p>
                 <p>Осадки: {this.props.rain}</p>
+
+                <p>Завтра</p>
+                <p>Температура: {this.props.temp1}</p>
+                <p>Осадки: {this.props.rain1}</p>
+
+
             </div>
         );
     }
