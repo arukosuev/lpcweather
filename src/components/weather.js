653270870fe8dc72189b566eead7509c9d328d87
Artyom <r.tem13@gmail.com>
2020-08-19 17:28:03 +0700

    реализованы ветер и его направление за 5дней

--- src/components/weather.js
+++ src/components/weather.js
@@ -3,18 +3,39 @@ import React from "react";
 class Weather extends React.Component {
     render() {
         return (
-            <div>
+            <div className="days">
                 <p>Город: {this.props.city}</p>
 
-                <p>Сегодня</p>
-                <p>Температура: {this.props.temp}</p>
-                <p>Осадки: {this.props.rain}</p>
-
-                <p>Завтра</p>
-                <p>Температура: {this.props.temp1}</p>
-                <p>Осадки: {this.props.rain1}</p>
-
-
+                <div className="day">
+                    <p>Сегодня</p>
+                    <p>Температура: {this.props.temp}</p>
+                    <p>Осадки: {this.props.rain}</p>
+                    <p>Ветер: {this.props.wind}, {this.props.dirwind}</p>
+                </div>
+                <div className="day">
+                    <p>Завтра</p>
+                    <p>Температура: {this.props.temp2}</p>
+                    <p>Осадки: {this.props.rain2}</p>
+                    <p>Ветер: {this.props.wind2}, {this.props.dirwind2}</p>
+                </div>
+                <div className="day">
+                    <p>3й день</p>
+                    <p>Температура: {this.props.temp3}</p>
+                    <p>Осадки: {this.props.rain3}</p>
+                    <p>Ветер: {this.props.wind3}, {this.props.dirwind3}</p>
+                </div>
+                <div className="day">
+                    <p>4й день</p>
+                    <p>Температура: {this.props.temp4}</p>
+                    <p>Осадки: {this.props.rain4}</p>
+                    <p>Ветер: {this.props.wind4}, {this.props.dirwind4}</p>
+                </div>
+                <div className="day">
+                    <p>5й день</p>
+                    <p>Температура: {this.props.temp5}</p>
+                    <p>Осадки: {this.props.rain5}</p>
+                    <p>Ветер: {this.props.wind5}, {this.props.dirwind5}</p>
+                </div>
             </div>
         );
     }
